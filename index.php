6dc0e6a808b867e3ec00fd75fbe9e2c0952cac71
Vincent Weinberger <vincevmw@mail.fresnostate.edu>
2020-12-13 13:14:41 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Main Menu</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="styles.css">
+	</head>
+	<body>
+		<div id="menu">
+			<img id="logo" src="images/battleshiplogo.png">
+			<h1>Main Menu</h1>
+			<ul id="menulist">
+				<a class="menulistitem" href="login.php"><li>Login</li></a>
+				<a class="menulistitem" href="register.php"><li>Register</li></a>
+				<a class="menulistitem" href="help.html"><li>Help</li></a>
+				<a class="menulistitem" href="about.html"><li>About</li></a>
+			</ul>
+		</div>
+		
+	</body>
+	<p id="copyright">&copy; VINCENT WEINBERGER - FRESNO STATE FALL SEMESTER 2020</p>
+</html>
